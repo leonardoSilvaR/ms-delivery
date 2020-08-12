38b167f68618977140353fcf624af2b1d1aaf8df
Leonardo S. Rodrigues <leonardo.silva.rodrigues2@gmail.com>
2020-08-12 02:53:41 -0300

    Correções deserialização

--- src/main/java/com/br/johndeere/msdelivery/integration/response/CServicoResponse.java
+++ src/main/java/com/br/johndeere/msdelivery/integration/response/CServicoResponse.java
@@ -1,15 +1,14 @@
 package com.br.johndeere.msdelivery.integration.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class CServicoResponse {
 
     @JsonProperty("Codigo")
--- src/main/java/com/br/johndeere/msdelivery/integration/response/ServicosResponse.java
+++ src/main/java/com/br/johndeere/msdelivery/integration/response/ServicosResponse.java
@@ -1,16 +1,17 @@
 package com.br.johndeere.msdelivery.integration.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServicosResponse {
 
+    @JsonProperty("cServico")
     private CServicoResponse cServico;
 
 }
